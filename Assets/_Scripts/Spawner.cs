499c6c09de257372932591403ba2248dd41dc0af
J2814 <j28x14@gmail.com>
2024-10-04 16:32:34 +0400

    spawner check

---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<Transform> SpawnPoints = new List<Transform>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
