e292b60ba35b08e7ab11c83785839c4abddcad7a
J2814 <j28x14@gmail.com>
2024-10-07 10:44:33 +0400

    спавнер чек

--- Assets/_Scripts/Spawner.cs
+++ Assets/_Scripts/Spawner.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +11,7 @@ public class Spawner : MonoBehaviour
     public Transform TargetHolder;
     void Start()
     {
-        
+        //ыловловл
     }
 
     void Update()
